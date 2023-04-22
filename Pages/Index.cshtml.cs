be34a3267781eea595ed8faa0ae38c22d9ea7977
MatiFrencia <88749621+MatiFrencia@users.noreply.github.com>
2023-04-22 12:09:40 -0300

    FLYWEIGHT LISTO.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,6 +15,7 @@ using Patrones_GOF.Services.Comportamiento.Iterator;
 using System.Runtime.CompilerServices;
 using Patrones_GOF.Services.Comportamiento.State;
 using Patrones_GOF.Services.Comportamiento.Strategy;
+using Patrones_GOF.Services.Comportamiento.Flyweight;
 
 namespace Patrones_GOF.Pages
 {
@@ -427,6 +428,40 @@ namespace Patrones_GOF.Pages
             jugador2.Disparar();
             jugador3.Disparar();
 
+            return Page();
+        }
+        public async Task<IActionResult> OnPostFlyweight()
+        {
+            // The client code usually creates a bunch of pre-populated
+            // flyweights in the initialization stage of the application.
+            var factory = new FlyweightFactory(
+                new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
+                new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
+                new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
+                new Car { Company = "BMW", Model = "M5", Color = "red" },
+                new Car { Company = "BMW", Model = "X6", Color = "white" }
+            );
+            factory.listFlyweights();
+
+            CarManager.addCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "M5",
+                Color = "red"
+            });
+
+            CarManager.addCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "X1",
+                Color = "red"
+            });
+
+            factory.listFlyweights();
             return Page();
         }
     }
---
+++ Services/Comportamiento/Flyweight/CarManager.cs
@@ -0,0 +1,22 @@
+﻿namespace Patrones_GOF.Services.Comportamiento.Flyweight
+{
+    public static class CarManager
+    {
+        public static void addCarToPoliceDatabase(FlyweightFactory factory, Car car)
+        {
+            Console.WriteLine("\nClient: Adding a car to database.");
+
+            var flyweight = factory.GetFlyweight(new Car
+            {
+                Color = car.Color,
+                Model = car.Model,
+                Company = car.Company
+            });
+
+            // The client code either stores or calculates extrinsic state and
+            // passes it to the flyweight's methods.
+            flyweight.Operation(car);
+        }
+    }
+
+}
